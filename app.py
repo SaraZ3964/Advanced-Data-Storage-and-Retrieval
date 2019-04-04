21437a53698a597239c523c6c2ddfc58baa42c54
Sirui Zhang <44712271+SaraZ3964@users.noreply.github.com>
2019-04-03 23:38:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,102 @@
 import datetime as dt
 import numpy as np
 import pandas as pd
-import datetime as dt
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite",connect_args={'check_same_thread': False}, echo=True)
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+Session = Session(engine)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+      return (
+        f"Available Routes for climate analysis!<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+  last_date = session.query(Measurements.date).order_by(Measurements.date.desc()).first()
+    last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    rain = session.query(Measurements.date, Measurements.prcp).\
+        filter(Measurements.date > last_year).\
+        order_by(Measurements.date).all()
+    rain_totals = []
+    
+    for result in rain:
+        row = {}
+        row["date"] = rain[0]
+        row["prcp"] = rain[1]
+        rain_totals.append(row)
+
+    return jsonify(rain_totals)
+  
+@app.route("/api/v1.0/stations")
+def stations():
+    stations_query = session.query(Station.name, Station.station)
+    stations = pd.read_sql(stations_query.statement, stations_query.session.bind)
+    return jsonify(stations.to_dict())
+  
+  @app.route("/api/v1.0/tobs")
+def tobs():
+  last_date = session.query(Measurements.date).order_by(Measurements.date.desc()).first()
+    last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    temperature = session.query(Measurements.date, Measurements.tobs).\
+        filter(Measurements.date > last_year).\
+        order_by(Measurements.date).all()
+    
+    F_totals = []
+    for result in temperature:
+        row = {}
+        row["date"] = temperature[0]
+        row["tobs"] = temperature[1]
+        F_totals.append(row)
+
+    return jsonify(temperature_totals)
+  
+  @app.route("/api/v1.0/<start>")
+def trip1(start):
+  start_date= dt.datetime.strptime(start, '%Y-%m-%d')
+    last_year = dt.timedelta(days=365)
+    start = start_date-last_year
+    end =  dt.date(2017, 8, 23)
+    trip_data = session.query(func.min(Measurements.tobs), func.avg(Measurements.tobs), func.max(Measurements.tobs)).\
+        filter(Measurements.date >= start).filter(Measurements.date <= end).all()
+    trip = list(np.ravel(trip_data))
+    return jsonify(trip)
+  
+  @app.route("/api/v1.0/<start>/<end>")
+def trip2(start,end):
+
+  # go back one year from start/end date and get Min/Avg/Max temp     
+    start_date= dt.datetime.strptime(start, '%Y-%m-%d')
+    end_date= dt.datetime.strptime(end,'%Y-%m-%d')
+    last_year = dt.timedelta(days=365)
+    start = start_date-last_year
+    end = end_date-last_year
+    trip_data = session.query(func.min(Measurements.tobs), func.avg(Measurements.tobs), func.max(Measurements.tobs)).\
+        filter(Measurements.date >= start).filter(Measurements.date <= end).all()
+    trip = list(np.ravel(trip_data))
+    return jsonify(trip)
+  
+  if __name__ == '__main__':
+    app.run(debug=True)
+  
